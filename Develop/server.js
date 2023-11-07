1057747f87473ab6e99a664f9b1c2b0bfc53c88c
romeo <rdumlao07@gmail.com>
2023-11-06 20:43:35 -0500

    //GET /api/notes route

--- Develop/server.js
+++ Develop/server.js
@@ -25,6 +25,26 @@ app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
     );
 
+//GET /api/notes route
+app.get('api/notes', (req, res) => {
+    //read db.json file
+    fs.readFile('db.json', 'utf8', (err, data) =>{
+        if (err) {
+            //Error
+            res.status(500).json({ error: 'Unable to read the notes'});
+        } else {
+            try {
+                //Parse the JSON data from the file
+                const notes = JSON.parse(data);
+                //Respond with notes as the JSON
+                res.json(notes);
+            } catch (error) {
+                //error for JSON parse
+            }
+        }
+    });
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
