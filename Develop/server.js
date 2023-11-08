098f69f18e4696e233c4eecb9ea4fca4affc1090
romeo <rdumlao07@gmail.com>
2023-11-07 22:02:58 -0500

    removed urlencoded

--- Develop/server.js
+++ Develop/server.js
@@ -13,7 +13,6 @@ const db = require('./db/db.json')
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET route for notes.html
